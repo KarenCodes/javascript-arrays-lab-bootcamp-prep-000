16288596ff1424bebf346624a7da7719017c304a
Karen Neumann <karen@websavvystudio.com>
2018-01-18 18:19:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var newArray = kittens.splice( 0, kittens.length-1 )
+  var newArray = kittens.slice( 0, kittens.length-1 )
   return newArray
 }
